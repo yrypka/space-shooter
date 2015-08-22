2d120aca1a6cf7d0244cd5dca7d77335f012f549
Yuriy Rypka <yurarypka@gmail.com>
2015-08-23 02:35:48 +0300

    07. Shooting Shots

--- Assets/Scripts/PlayerCotroller.cs
+++ Assets/Scripts/PlayerCotroller.cs
@@ -13,6 +13,18 @@ public class PlayerCotroller : MonoBehaviour {
 	public float tilt;
 	public Boundary boundary;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+	
+	void Update() {
+		if (Input.GetButton("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+	
 	void FixedUpdate () {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
